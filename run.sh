52d12cf663ba929500530aada1b5419092c5f150
lambertae <dengm@mit.edu>
2025-03-26 05:10:39 +0000

    important fix

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 # python main.py --base configs/latent-diffusion/ffhq-ldm-vq-4.yaml -t --gpus 0,1,2,3,4,5,6,7 \
 # 	2>&1 | tee main.log
 
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python main.py --base configs/latent-diffusion/ffhq-ldm-vq-4.yaml -t --gpus 0,1,2,3,4,5,6 \
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python main.py --base configs/latent-diffusion/ffhq-ldm-vq-4.yaml -t --gpus 0,1,2,3,4,5,6 --scale_lr False \
 	2>&1 | tee main.log
